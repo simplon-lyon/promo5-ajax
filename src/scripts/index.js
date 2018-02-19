9be6d80a35af29d7ffbebd6e46ff560bf0d82bcd
Jean <demeljean@hotmail.fr>
2018-02-19 12:00:40 +0100

    added first ajax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
 import '../scss/index.scss';
+import $ from 'jquery';
+
+$('.btn').on('click', function(){
+    $.ajax({
+        method: 'POST',
+        url: $('.url').val()
+    }).then(function(reponse) {
+        //fait ça une fois que t'as la réponse
+        $('p').text(reponse);
+    }).catch(function(error) {
+        //fait ça, si la requête a foirée
+        console.log('erreur');
+    });
+
+})
+
+
+
