3f5bed70ca37ede9396df146e0fcd1cf8a67e83d
Jean <demeljean@hotmail.fr>
2018-02-20 10:41:41 +0100

    added add person button

--- src/scripts/rest.js
+++ src/scripts/rest.js
@@ -7,3 +7,27 @@ $.ajax({
 }).catch(function(error) {
     console.error(error);  
 });
+
+$('button').on('click',function () {
+    let person = {
+        'name':'Pawel',
+        'birthdate':'2016/01/27',
+        'gender':0
+    };
+    $.ajax({
+        method: 'POST',
+        url:'http://localhost:8000/persons',
+        data: person
+    }).then(function(reponse) {
+        console.log(reponse);
+    }).catch(function(error) {
+        console.error(error);  
+    });
+});
+
+
+
+
+function display() {
+
+}
